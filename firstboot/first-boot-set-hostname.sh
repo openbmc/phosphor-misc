37c76da7c277f3531f35ec726ac690e53ab4915a
Patrick Williams <patrick@stwcx.xyz>
2022-12-08 06:23:48 -0600

    beautysh: re-format
    beautysh is enabled in the openbmc-build-scripts on Bash/Zsh/POSIX-sh
    files to have a consistent formatting.  Re-run the formatter on the
    whole repository.
    
    Change-Id: Ib1e9ab2fa4867e4f89401c97e3e4694fa3ea6765
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- firstboot/first-boot-set-hostname.sh
+++ firstboot/first-boot-set-hostname.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-show_error() {
+function show_error() {
     if [ -n "${JOURNAL_STREAM-}" ]; then
         echo "$@" | systemd-cat -t first-boot-set-hostname -p emerg
     else
@@ -8,7 +8,7 @@ show_error() {
     fi
 }
 
-sync_hostname() {
+function sync_hostname() {
     MAPPER_IFACE='xyz.openbmc_project.ObjectMapper'
     MAPPER_PATH='/xyz/openbmc_project/object_mapper'
     INVENTORY_PATH='/xyz/openbmc_project/inventory'
